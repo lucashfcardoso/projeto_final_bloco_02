ded16c7649ac4eba410a4c43c54929d43e81b6c7
lucashfcardoso <lucashfcardoso@gmail.com>
2024-05-03 11:37:09 -0300

    Branch 02 Finalizada

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Categoria } from './categoria/entities/categoria.entity';
+import { CategoriaModule } from './categoria/categoria.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
@@ -11,9 +13,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     username: 'root',
     password: 'root',
     database: 'db_farmacialucas',
-    entities: [],
+    entities: [Categoria],
     synchronize: true
-  })
+  }),
+  CategoriaModule
 ],
   controllers: [],
   providers: [],
